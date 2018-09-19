897caf7266c2db2c34c33fa995097f8bbc32e255
Osybear <osybeargames@gmail.com>
2018-09-18 19:57:46 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public BlockHandler blockHandler;
+	public LayerMask layerMask;
+	public Camera mainCamera;
+	
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hitInfo;
+        	if (Physics.Raycast(ray, out hitInfo, float.MaxValue, layerMask))
+			{
+				if(hitInfo.transform.name.Equals("Block")){
+					blockHandler.onMouseDown(hitInfo.transform.gameObject);
+				}
+			}
+		}
+	}
+}
